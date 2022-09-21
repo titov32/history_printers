6b0910807caa56ab0436954ce97cf5ddfd4a607b
Titov Evgeniy <titov32@gmail.com>
2022-09-21 11:48:23 +0700

    add favicon and fix bug show qr

--- app/hp/utils/qr.py
+++ app/hp/utils/qr.py
@@ -5,7 +5,7 @@ from app.config import DOMAIN_NAME
 def make_qr_code_by_path(path_to_printer_in_qr, name):
     text = f'{DOMAIN_NAME}{path_to_printer_in_qr}'
     img = qrcode.make(text)
-    file_name = f'static/qr/{name}.png'
+    file_name = f'app/static/qr/{name}.png'
     img.save(file_name)
     #url = f'http://{DOMAIN_NAME}/{file_name}'
     return file_name
