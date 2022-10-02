ca9a3057732127fbcaeb890366b24d6b48b1450c
Titov Evgeniy <titov32@gmail.com>
2022-10-02 21:13:16 +0700

    feat: added logging module for logging. Logging main function

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.hp.api_router import hp_api_router
 from app.hp.html_router import hp_html_router
+from app.hp.utils.logger import logger
+
 
 
 app = FastAPI(title="History Printers",
@@ -18,3 +20,13 @@ app.mount("/app/static", StaticFiles(directory="app/static"), name="app/static")
 
 app.include_router(hp_api_router)
 app.include_router(hp_html_router)
+
+@app.on_event("startup")
+async def on_startup():
+
+    logger.info('app startup')
+
+
+@app.on_event("shutdown")
+async def on_startup():
+    logger.info('app shutdown')
